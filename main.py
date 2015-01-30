b03ce0642c029a09d840132e25a0cb0d428054d2
Seth Liebert <mr.handlez@gmail.com>
2015-01-30 17:26:33 -0500

    Worked on pracing the input file. Also looked at the the champion and user module.

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
 import champion
-def main():
-	champion.championName = "Ziggs"
-	champion.championRole = "Mage"
+import user
 
-	print champion.championName
-	print champion.championRole
+def main():
+
+	user.userName = "TrulyInspired"
+	user.championPlayed = "Ziggs"
+	user.rolePlayed = "Mage"
+	user.lanePlayed = "Mid"
+	user.kills = 1
+	user.deaths = 1
+	user.assists = 1
+
+	#print user.userName
+	#print user.championPlayed
+	#print user.rolePlayed
+	#print user.lanePlayed
+	#print user.kills
+	#print user.deaths
+	#print user.assists
+
+	file = open("championList.txt",'r')
+	row = file.readlines()
+
+	for char in row:
+		print char
+	#s = row.split()
+
+
+	#for line in row:
+		
+
+	#file = open('newfile.txt','w')
+	#file.close
+
+
 main()
